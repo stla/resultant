446c2f207a3b7f42c14e3189f868953384cefaf2
stla <laurent_step@yahoo.fr>
2024-03-15 01:06:44 +0100

    curves01

--- inst/essais/curves01.R
+++ inst/essais/curves01.R
@@ -6,11 +6,12 @@ g <- function(x, y) {
   y - 2*x^2
 }
 
+# contour line for f(x,y)=0
 x <- seq(-1.2, 1.2, len = 2000)
 y <- seq(0, 1, len = 2000)
 zf <- outer(x, y, f)
 crf <- contourLines(x, y, zf, levels = 0)
-
+# contour line for g(x,y)=0
 x <- seq(-1, 1, len = 2000)
 y <- seq(0, 1.5, len = 2000)
 zg <- outer(x, y, g)
@@ -23,13 +24,14 @@ intercalateNA <- function(xs) {
     c(xs[[1L]], NA, intercalateNA(xs[-1L]))
   }
 }
+contourData <- function(cr) {
+  xs <- lapply(cr, `[[`, "x")
+  ys <- lapply(cr, `[[`, "y")
+  data.frame("x" = intercalateNA(xs), "y" = intercalateNA(ys))
+}
 
-xs <- lapply(crf, `[[`, "x")
-ys <- lapply(crf, `[[`, "y")
-datf <- data.frame(x = intercalateNA(xs), y = intercalateNA(ys))
-xs <- lapply(crg, `[[`, "x")
-ys <- lapply(crg, `[[`, "y")
-datg <- data.frame(x = intercalateNA(xs), y = intercalateNA(ys))
+datf <- contourData(crf)
+datg <- contourData(crg)
 
 library(ggplot2)
 ggplot() +
@@ -37,22 +39,21 @@ ggplot() +
   geom_path(aes(x, y), data = datg, linewidth = 1, color = "green")
 
 ###
+# define the two polynomials
 library(qspray)
-library(resultant)
-
 x <- qlone(1)
 y <- qlone(2)
 p <- f(x, y)
 q <- g(x, y)
 
-R <- resultant(p, q, var = 1)
-prettyQspray(R, vars = "x")
+Rx <- resultant::resultant(p, q, var = 1) # var=1 <=> var="x"
+prettyQspray(Rx, vars = "x")
 #coeffs <- sapply(0:8, function(k) gmp::asNumeric(getCoefficient(R, k)))
 
 
 library(giacR)
 giac <- Giac$new()
-command <- sprintf("roots(%s)", prettyQspray(R, vars = "x"))
+command <- sprintf("roots(%s)", prettyQspray(Rx, vars = "x"))
 giac$execute(command)
 giac$close()
 
